461012a5a911954f46a0ee1e898c09bad87c4114
Deepesh Patil <deepeshvcd6273@gmail.com>
2024-01-13 01:32:19 +0530

    Fixing Issues

---
+++ !@#$%/dist/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.6 (trial), 000000, 2024-01-13T01:30:14.045562
+from .pyarmor_runtime import __pyarmor__
--- !@#$%/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 8.4.6 (trial), 000000, 2024-01-01T14:24:53.845459
-from .pyarmor_runtime import __pyarmor__
