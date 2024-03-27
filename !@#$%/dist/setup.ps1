b5fba74ccc0f83b625d318c8b4fa275c6f9ac2f3
Deepesh Patil <deepeshvcd6273@gmail.com>
2024-03-27 18:04:53 +0530

    Adding 3DES for text encryption and decryption

--- !@#$%/dist/setup.ps1
+++
@@ -1,9 +0,0 @@
-
-cd ..
-
-pip install -r requirements.txt
-
-./setup.py
-
-pyarmor gen ./setup.py ./main.py ./lib.py
-
---
+++ setup.ps1
@@ -0,0 +1,9 @@
+
+cd ./src
+
+pip install -r requirements.txt
+
+./setup.py
+
+pyarmor gen ./setup.py ./main.py ./lib.py
+
